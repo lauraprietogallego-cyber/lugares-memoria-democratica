22ea44d83a49fa6da1fbfd63c0e8fcdcde9ba99b
lauraprietogallego-cyber <lauraprietogallego@gmail.com>
2026-07-18 21:28:22 +0200

    scrip cambios

--- script.js
+++ script.js
@@ -4,4 +4,32 @@ const mapa = new maplibregl.Map({
     center: [-3.7038, 40.4168],
     zoom: 5
 });
+
+
 mapa.addControl(new maplibregl.NavigationControl());
+
+
+// Cargar lugares de memoria
+
+fetch('datos/lugares.json')
+    .then(response => response.json())
+    .then(lugares => {
+
+        lugares.forEach(lugar => {
+
+            const marcador = new maplibregl.Marker({
+                color: '#b50e0e'
+            })
+            .setLngLat([lugar.lon, lugar.lat])
+            .setPopup(
+                new maplibregl.Popup()
+                .setHTML(`
+                    <h3>${lugar.nombre}</h3>
+                    <p>${lugar.descripcion}</p>
+                `)
+            )
+            .addTo(mapa);
+
+        });
+
+    });
