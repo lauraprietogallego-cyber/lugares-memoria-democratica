80e10096c6977cf026a40f7725179309ef3c843d
lauraprietogallego-cyber <lauraprietogallego@gmail.com>
2026-07-18 21:29:59 +0200

    asd

--- script.js
+++ script.js
@@ -1,33 +1,15 @@
-const mapa = new maplibregl.Map({
-    container: 'mapa-interactivo',
-    style: 'https://demotiles.maplibre.org/style.json',
-    center: [-3.7038, 40.4168],
-    zoom: 5
-});
-
-
-mapa.addControl(new maplibregl.NavigationControl());
-
-
-// Cargar lugares de memoria
-
 fetch('datos/lugares.json')
     .then(response => response.json())
     .then(lugares => {
 
+        console.log(lugares);
+
         lugares.forEach(lugar => {
 
-            const marcador = new maplibregl.Marker({
+            new maplibregl.Marker({
                 color: '#b50e0e'
             })
             .setLngLat([lugar.lon, lugar.lat])
-            .setPopup(
-                new maplibregl.Popup()
-                .setHTML(`
-                    <h3>${lugar.nombre}</h3>
-                    <p>${lugar.descripcion}</p>
-                `)
-            )
             .addTo(mapa);
 
         });
